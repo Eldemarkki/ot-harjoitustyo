49bebada7d78cc45596a1a437bf788634840c8fc
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-11-03 14:31:14 +0200

    Tehtävät 1-3

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,47 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        self.kortti = Maksukortti(1000)
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
+
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_edullisesti()
+
+        self.assertEqual(self.kortti.saldo_euroina(), 7.5)
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_maukkaasti()
+
+        self.assertEqual(self.kortti.saldo_euroina(), 6.0)
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
+
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
+
+    def test_negatiivinen_lataussumma_ei_muuta_saldoa(self):
+        self.kortti.lataa_rahaa(-50)
+        self.assertEqual(self.kortti.saldo_euroina(), 10)
+
+    def test_voi_syoda_edullisesti_kun_kortilla_tasmalleen_oikea_maara(self):
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+        self.assertEqual(kortti.saldo_euroina(), 0)
+
+    
+    def test_voi_syoda_maukkaasti_kun_kortilla_tasmalleen_oikea_maara(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+        self.assertEqual(kortti.saldo_euroina(), 0)

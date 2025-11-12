49fa1bd5339de808f30df202bd47feafea2c0a4b
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-11-12 12:28:35 +0200

    feat: implement simulation rules

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -27,6 +27,63 @@ class GameOfLife:
         for offset in glider:
             self._cells[y + offset[1]][x + offset[0]] = True
 
+    def get_live_neighbors(self, x, y):
+        offsets = [-1, 0, 1]
+
+        neighbors = []
+        for y_offset in offsets:
+            for x_offset in offsets:
+                # The cell itself is not included in the neighbors
+                if y_offset == 0 and x_offset == 0:
+                    continue
+
+                # If it's out of bounds, consider the neighbor dead
+                nx = x + x_offset
+                ny = y + y_offset
+                if nx < 0 or nx > self.get_width() - 1 or ny < 0 or ny > self.get_height() - 1:
+                    continue
+                
+                if self.get_cell(nx, ny):
+                    neighbors.append([nx, ny])
+        
+        return neighbors
+
+    def _simulate_cell(self, x, y):
+        alive = self.get_cell(x, y)
+        neighbors = self.get_live_neighbors(x, y)
+
+        # "Any live cell with fewer than two live neighbours dies, as if by underpopulation."
+        if alive and len(neighbors) < 2:
+            return False
+
+        # "Any live cell with two or three live neighbours lives on to the next generation."
+        if alive and (len(neighbors) == 2 or len(neighbors) == 3):
+            return True
+
+        # "Any live cell with more than three live neighbours dies, as if by overpopulation."
+        if alive and len(neighbors) > 3:
+            return False
+
+        # "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction."
+        if not alive and len(neighbors) == 3:
+            return True
+
+        # (dead cells remain dead)
+        return False
+
+
+    def simulate(self):
+        new_cells = []
+
+        for y in range(self.get_height()):
+            row = []
+            for x in range(self.get_width()):
+                new_state = self._simulate_cell(x, y)
+                row.append(new_state)
+            new_cells.append(row)
+
+        self._cells = new_cells
+
     def __str__(self):
         text = ""
         for y in range(self.get_height()):
